9b48d4fda02479441e1344c761ec1be4d7fd8ff3
eakgul9 <eylul.akgul9@gmail.com>
2022-02-01 10:17:34 -0800

    Initial Commit

---
+++ Assignment0210/Assignment0210App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment0210App.swift
+//  Assignment0210
+//
+//  Created by eylul on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment0210App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
